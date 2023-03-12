ef56e94b8b2a7ad9a9be932d5eab86076ade16ff
Chris Miller <chris@rajapet.net>
2023-03-12 16:17:16 -0400

    Initial code
    Initial code

---
+++ WsaPermissions/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Storage;
+
+namespace WsaPermissions;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnFolderBtnClicked(object sender, EventArgs e) {
+        CancellationTokenSource source = new();
+        CancellationToken cancellationToken = source.Token;
+        var folderPickerResult = await FolderPicker.PickAsync(cancellationToken);
+        if (folderPickerResult.IsSuccessful) {
+            await Toast.Make($"Folder picked: Name - {folderPickerResult.Folder.Name}, Path - {folderPickerResult.Folder.Path}", ToastDuration.Long).Show(cancellationToken);
+        }
+        else {
+            await Toast.Make($"Folder is not picked, {folderPickerResult.Exception.Message}").Show(cancellationToken);
+        }
+    }
+}
+
---
+++ WsaPermissions/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WsaPermissions;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
